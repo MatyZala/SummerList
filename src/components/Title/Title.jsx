ce06790f312327aae0c792ab39d0b81f089f5fc2
MatyZala <mzx631gmail.com>
2023-04-17 21:48:14 -0300

    SummerList

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+const Title = () => {
+    return (
+        <h1 className="text-5xl font-anton font-bold tracking-widest">
+            Summer Beach 101
+        </h1>
+    );
+}
+
+export { Title }
